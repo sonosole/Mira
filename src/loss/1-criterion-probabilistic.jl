ec75644e803edd3c3bac650dc33648535295330b
sole <taiyang_zhao@163.com>
2022-04-15 00:49:00 +0800

    style: arguments' name, not important

--- src/loss/1-criterion-probabilistic.jl
+++ src/loss/1-criterion-probabilistic.jl
@@ -11,39 +11,40 @@ export binaryCrossEntropyLoss
     crossEntropy(̂p::Variable{T}, p::Variable{T}) -> y::Variable{T}
 cross entropy is `y = - p * log(̂p)` where p is target and ̂p is the output of the network.
 """
-function crossEntropy(x::Variable{T}, label::Variable{T}) where T
-    @assert (x.shape == label.shape)
-    backprop = (x.backprop || label.backprop)
-    ϵ = eltype(x)(1e-38)
-    y = Variable{T}(- ᵛ(label) .* log.(ᵛ(x) .+ ϵ), backprop)
+function crossEntropy(p::Variable{T}, 𝜌::Variable{T}) where T
+    @assert (p.shape == 𝜌.shape)
+    backprop = (p.backprop || 𝜌.backprop)
+    ϵ = eltype(p)(1e-38)
+    y = Variable{T}(- ᵛ(𝜌) .* log.(ᵛ(p) .+ ϵ), backprop)
     if backprop
         y.backward = function crossEntropyBackward()
-            if need2computeδ!(x)
-                δ(x) .-= δ(y) .* ᵛ(label) ./ (ᵛ(x) .+ ϵ)
+            if need2computeδ!(p)
+                δ(p) .-= δ(y) .* ᵛ(𝜌) ./ (ᵛ(p) .+ ϵ)
             end
             ifNotKeepδThenFreeδ!(y)
         end
-        addchild(y, x)
+        addchild(y, p)
     end
     return y
 end
+
 
 """
     crossEntropy(̂p::Variable{T}, p::Variable{T}) -> y::Variable{T}
 cross entropy is `y = - p * log(̂p)` where p is target and ̂p is the output of the network.
 """
-function crossEntropy(x::Variable{T}, label::T) where T
-    @assert x.shape == size(label)
-    ϵ = eltype(x)(1e-38)
-    y = Variable{T}(- label .* log.(ᵛ(x) .+ ϵ), x.backprop)
+function crossEntropy(p::Variable{T}, 𝜌::T) where T
+    @assert p.shape == size(𝜌)
+    ϵ = eltype(p)(1e-38)
+    y = Variable{T}(- 𝜌 .* log.(ᵛ(p) .+ ϵ), p.backprop)
     if y.backprop
         y.backward = function crossEntropyBackward()
-            if need2computeδ!(x)
-                δ(x) .-= δ(y) .* label ./ (ᵛ(x) .+ ϵ)
+            if need2computeδ!(p)
+                δ(p) .-= δ(y) .* 𝜌 ./ (ᵛ(p) .+ ϵ)
             end
             ifNotKeepδThenFreeδ!(y)
         end
-        addchild(y, x)
+        addchild(y, p)
     end
     return y
 end
@@ -57,25 +58,25 @@ crossEntropyLoss(x::Variable{T}, label::T; reduction::String="sum") where T = lo
     binaryCrossEntropy(̂p::Variable{T}, p::Variable{T}) -> y::Variable{T}
 binary cross entropy is `y = - plog(̂p) - (1-p)log(1-̂p)` where p is target and ̂p is the output of the network.
 """
-function binaryCrossEntropy(x::Variable{T}, label::Variable{T}) where T
-    @assert (x.shape == label.shape)
-    backprop = (x.backprop || label.backprop)
-    TOO  = eltype(x)
+function binaryCrossEntropy(p::Variable{T}, 𝜌::Variable{T}) where T
+    @assert (p.shape == 𝜌.shape)
+    backprop = (p.backprop || 𝜌.backprop)
+    TOO  = eltype(p)
     ϵ  = TOO(1e-38)
     𝟙  = TOO(1.0f0)
-    tmp1 = - ᵛ(label) .* log.(ᵛ(x) .+ ϵ)
-    tmp2 = - (𝟙 .- ᵛ(label)) .* log.(𝟙 .- ᵛ(x) .+ ϵ)
-    y  = Variable{T}(tmp1 + tmp2, backprop)
+    t₁ = -       ᵛ(𝜌)  .* log.(     ᵛ(p) .+ ϵ)
+    t₂ = - (𝟙 .- ᵛ(𝜌)) .* log.(𝟙 .- ᵛ(p) .+ ϵ)
+    y  = Variable{T}(t₁ + t₂, backprop)
     if backprop
         y.backward = function binaryCrossEntropyBackward()
-            if need2computeδ!(x)
-                temp1 = (𝟙 .- ᵛ(label)) ./ (𝟙 .- ᵛ(x) .+ ϵ)
-                temp2 =       ᵛ(label)  ./ (     ᵛ(x) .+ ϵ)
-                δ(x) .+= δ(y) .* (temp1 - temp2)
+            if need2computeδ!(p)
+                δ₁ = (𝟙 .- ᵛ(𝜌)) ./ (𝟙 .- ᵛ(p) .+ ϵ)
+                δ₂ =       ᵛ(𝜌)  ./ (     ᵛ(p) .+ ϵ)
+                δ(p) .+= δ(y) .* (δ₁ - δ₂)
             end
             ifNotKeepδThenFreeδ!(y);
         end
-        addchild(y, x)
+        addchild(y, p)
     end
     return y
 end
@@ -85,24 +86,24 @@ end
     binaryCrossEntropy(̂p::Variable{T}, p::Variable{T}) -> y::Variable{T}
 binary cross entropy is `y = - plog(̂p) - (1-p)log(1-̂p)` where p is target and ̂p is the output of the network.
 """
-function binaryCrossEntropy(x::Variable{T}, label::T) where T
-    @assert x.shape == size(label.shape)
-    TO = eltype(x)
+function binaryCrossEntropy(p::Variable{T}, 𝜌::T) where T
+    @assert p.shape == size(𝜌.shape)
+    TO = eltype(p)
     ϵ  = TO(1e-38)
     𝟙  = TO(1.0f0)
-    tmp1 = - label .* log.(ᵛ(x) .+ ϵ)
-    tmp2 = - (𝟙 .- label) .* log.(𝟙 .- ᵛ(x) .+ ϵ)
-    y  = Variable{T}(tmp1 + tmp2, x.backprop)
+    t₁ = -       𝜌  .* log.(     ᵛ(p) .+ ϵ)
+    t₂ = - (𝟙 .- 𝜌) .* log.(𝟙 .- ᵛ(p) .+ ϵ)
+    y  = Variable{T}(t₁ + t₂, p.backprop)
     if y.backprop
         y.backward = function binaryCrossEntropyBackward()
-            if need2computeδ!(x)
-                temp1 = (𝟙 .- label) ./ (𝟙 .- ᵛ(x) .+ ϵ)
-                temp2 =       label  ./ (     ᵛ(x) .+ ϵ)
-                δ(x) .+= δ(y) .* (temp1 - temp2)
+            if need2computeδ!(p)
+                δ₁ = (𝟙 .- 𝜌) ./ (𝟙 .- ᵛ(p) .+ ϵ)
+                δ₂ =       𝜌  ./ (     ᵛ(p) .+ ϵ)
+                δ(p) .+= δ(y) .* (δ₁ - δ₂)
             end
             ifNotKeepδThenFreeδ!(y)
         end
-        addchild(y, x)
+        addchild(y, p)
     end
     return y
 end
