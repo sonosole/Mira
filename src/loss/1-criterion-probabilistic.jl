eac1c55aa5a95bc68d5fb786fc1a3270539e1927
sole <taiyang_zhao@163.com>
2022-05-06 00:32:20 +0800

    feat: focalCELoss

--- src/loss/1-criterion-probabilistic.jl
+++ src/loss/1-criterion-probabilistic.jl
@@ -6,23 +6,27 @@ export crossEntropyLoss
 export binaryCrossEntropy
 export binaryCrossEntropyLoss
 
+export focalCE
+export focalCELoss
 export focalBCE
 export focalBCELoss
 
 
 """
-    crossEntropy(p::Variable{T}, 𝜌::Variable{T}) -> y::Variable{T}
-cross entropy is `y = - 𝜌 * log(p)` where 𝜌 is the target and p is the output of the network.
+    crossEntropy(p::Variable{T}, label::Variable{T}) -> y::Variable{T}
+cross entropy is `y = - label * log(p)` where label is the target and p is the output of the network.
 """
-function crossEntropy(p::Variable{T}, 𝜌::Variable{T}) where T
-    @assert (p.shape == 𝜌.shape)
-    backprop = (p.backprop || 𝜌.backprop)
+function crossEntropy(p::Variable{T}, label::Variable{T}) where T
+    @assert (p.shape == label.shape)
+    backprop = (p.backprop || label.backprop)
+    𝝆 = ᵛ(label)
+    𝒑 = ᵛ(p)
     ϵ = eltype(p)(1e-38)
-    y = Variable{T}(- ᵛ(𝜌) .* log.(ᵛ(p) .+ ϵ), backprop)
+    y = Variable{T}(- 𝝆 .* log.(𝒑 .+ ϵ), backprop)
     if backprop
         y.backward = function crossEntropyBackward()
             if need2computeδ!(p)
-                δ(p) .-= δ(y) .* ᵛ(𝜌) ./ (ᵛ(p) .+ ϵ)
+                δ(p) .-= δ(y) .* 𝝆 ./ (𝒑 .+ ϵ)
             end
             ifNotKeepδThenFreeδ!(y)
         end
@@ -33,17 +37,19 @@ end
 
 
 """
-    crossEntropy(p::Variable{T}, 𝜌::AbstractArray) -> y::Variable{T}
-cross entropy is `y = - 𝜌 * log(p)` where 𝜌 is the target and p is the output of the network.
+    crossEntropy(p::Variable{T}, label::AbstractArray) -> y::Variable{T}
+cross entropy is `y = - label * log(p)` where label is the target and p is the output of the network.
 """
-function crossEntropy(p::Variable{T}, 𝜌::AbstractArray) where T
-    @assert p.shape == size(𝜌)
+function crossEntropy(p::Variable{T}, label::AbstractArray) where T
+    @assert p.shape == size(label)
+    𝝆 = label
+    𝒑 = ᵛ(p)
     ϵ = eltype(p)(1e-38)
-    y = Variable{T}(- 𝜌 .* log.(ᵛ(p) .+ ϵ), p.backprop)
+    y = Variable{T}(- 𝝆 .* log.(𝒑 .+ ϵ), p.backprop)
     if y.backprop
         y.backward = function crossEntropyBackward()
             if need2computeδ!(p)
-                δ(p) .-= δ(y) .* 𝜌 ./ (ᵛ(p) .+ ϵ)
+                δ(p) .-= δ(y) .* 𝝆 ./ (𝒑 .+ ϵ)
             end
             ifNotKeepδThenFreeδ!(y)
         end
@@ -66,26 +72,28 @@ end
 
 
 """
-    binaryCrossEntropy(p::Variable{T}, 𝜌::Variable{T}) -> y::Variable{T}
-binary cross entropy is `y = - 𝜌log(p) - (1-𝜌)log(1-p)` where 𝜌 is the target and p is the output of the network.
+    binaryCrossEntropy(p::Variable{T}, label::Variable{T}) -> y::Variable{T}
+binary cross entropy is `y = - label*log(p) - (1-label)log(1-p)` where 𝜌 is the target and p is the output of the network.
 """
-function binaryCrossEntropy(p::Variable{T}, 𝜌::Variable{T}) where T
-    @assert (p.shape == 𝜌.shape)
-    backprop = (p.backprop || 𝜌.backprop)
-    TOO  = eltype(p)
-    ϵ  = TOO(1e-38)
-    𝟙  = TOO(1.0f0)
-    t₁ = -       ᵛ(𝜌)  .* log.(     ᵛ(p) .+ ϵ)
-    t₂ = - (𝟙 .- ᵛ(𝜌)) .* log.(𝟙 .- ᵛ(p) .+ ϵ)
+function binaryCrossEntropy(p::Variable{T}, label::Variable{T}) where T
+    @assert (p.shape == label.shape)
+    backprop = (p.backprop || label.backprop)
+    TO = eltype(p)
+    ϵ  = TO(1e-38)
+    𝟙  = TO(1.0f0)
+    𝝆  = ᵛ(label)
+    𝒑  = ᵛ(p)
+    t₁ = @. -      𝝆  * log(    𝒑 + ϵ)
+    t₂ = @. - (𝟙 - 𝝆) * log(𝟙 - 𝒑 + ϵ)
     y  = Variable{T}(t₁ + t₂, backprop)
     if backprop
         y.backward = function binaryCrossEntropyBackward()
             if need2computeδ!(p)
-                δ₁ = (𝟙 .- ᵛ(𝜌)) ./ (𝟙 .- ᵛ(p) .+ ϵ)
-                δ₂ =       ᵛ(𝜌)  ./ (     ᵛ(p) .+ ϵ)
+                δ₁ = @. (𝟙 - 𝝆) / (𝟙 - 𝒑 + ϵ)
+                δ₂ = @.      𝝆  / (    𝒑 + ϵ)
                 δ(p) .+= δ(y) .* (δ₁ - δ₂)
             end
-            ifNotKeepδThenFreeδ!(y);
+            ifNotKeepδThenFreeδ!(y)
         end
         addchild(y, p)
     end
@@ -95,21 +103,23 @@ end
 
 """
     binaryCrossEntropy(p::Variable{T}, 𝜌::AbstractArray) -> y::Variable{T}
-binary cross entropy is `y = - 𝜌log(p) - (1-𝜌)log(1-p)` where 𝜌 is the target and p is the output of the network.
+binary cross entropy is `y = - label*log(p) - (1-label)log(1-p)` where 𝜌 is the target and p is the output of the network.
 """
-function binaryCrossEntropy(p::Variable{T}, 𝜌::AbstractArray) where T
-    @assert p.shape == size(𝜌)
+function binaryCrossEntropy(p::Variable{T}, label::AbstractArray) where T
+    @assert p.shape == size(label)
     TO = eltype(p)
     ϵ  = TO(1e-38)
     𝟙  = TO(1.0f0)
-    t₁ = -       𝜌  .* log.(     ᵛ(p) .+ ϵ)
-    t₂ = - (𝟙 .- 𝜌) .* log.(𝟙 .- ᵛ(p) .+ ϵ)
+    𝝆  = label
+    𝒑  = ᵛ(p)
+    t₁ = @. -      𝝆  * log(    𝒑 + ϵ)
+    t₂ = @. - (𝟙 - 𝝆) * log(𝟙 - 𝒑 + ϵ)
     y  = Variable{T}(t₁ + t₂, p.backprop)
     if y.backprop
         y.backward = function binaryCrossEntropyBackward()
             if need2computeδ!(p)
-                δ₁ = (𝟙 .- 𝜌) ./ (𝟙 .- ᵛ(p) .+ ϵ)
-                δ₂ =       𝜌  ./ (     ᵛ(p) .+ ϵ)
+                δ₁ = @. (𝟙 - 𝝆) / (𝟙 - 𝒑 + ϵ)
+                δ₂ = @.      𝝆  / (    𝒑 + ϵ)
                 δ(p) .+= δ(y) .* (δ₁ - δ₂)
             end
             ifNotKeepδThenFreeδ!(y)
@@ -129,8 +139,8 @@ function binaryCrossEntropy(p::AbstractArray, label::AbstractArray)
     TO = eltype(p)
     ϵ  = TO(1e-38)
     𝟙  = TO(1.0f0)
-    t₁ = -       label  .* log.(     p .+ ϵ)
-    t₂ = - (𝟙 .- label) .* log.(𝟙 .- p .+ ϵ)
+    t₁ = @. -      label  * log(    p + ϵ)
+    t₂ = @. - (𝟙 - label) * log(𝟙 - p + ϵ)
     return t₁ + t₂
 end
 
@@ -144,17 +154,18 @@ binaryCrossEntropyLoss(x::Variable{T}, label::AbstractArray; reduction::String="
 binaryCrossEntropyLoss(x::AbstractArray, label::AbstractArray; reduction::String="sum") = loss(binaryCrossEntropy(x, label), reduction=reduction)
 
 
-function focalBCE(p::Variable{T}, 𝜌::AbstractArray; gamma::Real=2, alpha::Real=0.5) where T
+function focalBCE(p::Variable{T}, label::AbstractArray; gamma::Real=2, alpha::Real=0.5) where T
     @assert p.shape == size(𝜌)
     TO = eltype(p)
     ϵ  = TO(1e-38)
     𝟙  = TO(1.0f0)
-    γ  = gamma
-    α  = alpha
+    γ  = TO(gamma)
+    α  = TO(alpha)
+    𝝆  = label
     𝒑  = ᵛ(p)
 
-    w₁ = @. -      α  *      𝜌
-    w₂ = @. - (𝟙 - α) * (𝟙 - 𝜌)
+    w₁ = @. -      α  *      𝝆
+    w₂ = @. - (𝟙 - α) * (𝟙 - 𝝆)
 
     t₁ = @. w₁ * (𝟙 - 𝒑)^ γ * log(    𝒑 + ϵ)
     t₂ = @. w₂ *      𝒑 ^ γ * log(𝟙 - 𝒑 + ϵ)
@@ -164,7 +175,7 @@ function focalBCE(p::Variable{T}, 𝜌::AbstractArray; gamma::Real=2, alpha::Rea
     if y.backprop
         y.backward = function focalBCEBackward()
             if need2computeδ!(p)
-                δ₁ = @. w₁ * (𝟙 - 𝒑)^(γ-1) * (𝟙 / 𝒑 - γ * log(𝒑) - 𝟙)
+                δ₁ = @. w₁ * (𝟙 - 𝒑)^(γ - 𝟙) * (𝟙 / 𝒑 - γ * log(𝒑) - 𝟙)
                 δ₂ = @. w₂ * 𝒑 ^ γ * (𝟙 / (𝒑 - 𝟙) + γ * log(𝟙 - 𝒑) / 𝒑)
                 δ(p) .+= δ(y) .* (δ₁ + δ₂)
             end
@@ -176,4 +187,31 @@ function focalBCE(p::Variable{T}, 𝜌::AbstractArray; gamma::Real=2, alpha::Rea
 end
 
 
-focalBCELoss(x::Variable{T}, label::AbstractArray; reduction::String="sum") where T = loss(focalBCE(x, label), reduction=reduction)
+function focalCE(p::Variable{T}, label::AbstractArray; gamma::Real=2) where T
+    @assert p.shape == size(𝜌)
+    TO = eltype(p)
+    ϵ  = TO(1e-38)
+    𝟙  = TO(1.0f0)
+    γ  = TO(gamma)
+    𝝆  = label
+    𝒑  = ᵛ(p)
+
+    t = @. 𝝆 * (𝟙 - 𝒑) ^ γ * log(𝒑 + ϵ)
+    y = Variable{T}(t, p.backprop)
+
+    if y.backprop
+        y.backward = function focalCEBackward()
+            if need2computeδ!(p)
+                δ = @. 𝝆 * (𝟙 - 𝒑)^(γ - 𝟙) * (𝟙 / 𝒑 - γ * log(𝒑) - 𝟙)
+                δ(p) .+= δ(y) .* δ
+            end
+            ifNotKeepδThenFreeδ!(y)
+        end
+        addchild(y, p)
+    end
+    return y
+end
+
+
+focalCELoss(x::Variable{T}, label::AbstractArray; gamma::Real=2, reduction::String="sum") where T = loss(focalCE(x, label, gamma=gamma), reduction=reduction)
+focalBCELoss(x::Variable{T}, label::AbstractArray; gamma::Real=2, reduction::String="sum") where T = loss(focalBCE(x, label, gamma=gamma), reduction=reduction)
