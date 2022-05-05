69436e39499d2cdcb5eaf31b9093617411e6ef17
Sole <30993566+sonosole@users.noreply.github.com>
2022-05-05 11:18:04 +0800

    Update include.jl

--- src/block/include.jl
+++ src/block/include.jl
@@ -27,3 +27,66 @@ include("./conv/include.jl")
 include("./fc/include.jl")
 include("./rnn/include.jl")
 include("./pfun/include.jl")
+
+
+const FC = Union{Affine,
+                 Linear,
+                 Dense,
+                 Maxout,
+                 Res0d,
+                 Res0dWithBN,
+                 SelfLoopResNet,
+                 SelfLoopCumulativeResNet,
+                 MeanNormResDense
+}
+
+
+export Keep3dimsForward
+export Keep3dimsPredict
+export KeepNdimsForward
+export KeepNdimsPredict
+
+
+function Keep3dimsForward(block::FC, x::Variable)
+    C,T,B = size(x)
+    y = reshape(x, (C, T*B))  # 3D --> 2D
+    z = forward(block, y)
+    C, TB = size(z)
+    return reshape(z, (C,T,B))  # 2D --> 3D
+end
+
+
+function Keep3dimsPredict(block::FC, x::AbstractArray)
+    C,T,B = size(x)
+    y = reshape(x, (C, T*B))  # 3D --> 2D
+    z = predict(block, y)
+    C, TB = size(z)
+    return reshape(z, (C,T,B))  # 2D --> 3D
+end
+
+
+function KeepNdimsForward(block::FC, x::Variable)
+    S = size(x)             # N-Dim Variable
+    F = S[1]                # Feat-Dim
+    B = prod(S[2:end])      # Batch-Dim
+    y = reshape(x, (F, B))  # N-D to 2D
+    z = forward(block, y)   # forward once
+
+    F, B = size(z)          # Feat-Dim, Batch-Dim
+    V = ntuple(i -> i≠1 ? S[i] : F, length(S))
+    return reshape(z, V)    # 2D to N-D
+end
+
+
+function KeepNdimsPredict(block::FC, x::Variable)
+    S = size(x)             # N-Dim Variable
+    F = S[1]                # Feat-Dim
+    B = prod(S[2:end])      # Batch-Dim
+    y = reshape(x, (F, B))  # N-D to 2D
+    z = predict(block, y)   # forward once
+
+    F, B = size(z)          # Feat-Dim, Batch-Dim
+    V = ntuple(i -> i≠1 ? S[i] : F, length(S))
+    return reshape(z, V)    # 2D to N-D
+end
+
