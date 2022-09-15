a37bc3d76fa3425e890dd2f2e8fa9cf899b17317
sole <taiyang_zhao@163.com>
2022-09-16 01:24:47 +0800

    refactor: auc for new input types
    auc(positives::Vector, negatives::Vector)

--- src/metrics/binary/auc.jl
+++ src/metrics/binary/auc.jl
@@ -1,12 +1,21 @@
-export auc
+export auc, aucpair
 
 
 """
-    auc(pred::AbstractArray, label::AbstractArray)
-compute Area Under the Curve : https://arize.com/blog/what-is-auc/
+    auc(pred::Vector{<:AbstractFloat}, label::Vector{Int})
 
+Compute Area Under the Curve : https://arize.com/blog/what-is-auc/\n
+AUC represents the probability of positive-sample-score ≥ negative-sample-score. i.e.\n
+
+            number of (sample⁺, sample⁻) pairs that
+            satisfies sample⁺ score ≥ sample⁻ score
+    AUC = ───────────────────────────────────────────
+              count of (sample⁺, sample⁻) pairs
+
+    if sample⁺ score  > sample⁻ score, then number += 1
+    if sample⁺ score == sample⁻ score, then number += 1/2
 ```
-julia> label = [1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0];
+julia> label = [1  , 0  , 1  , 0  , 0  , 0  , 1  , 0  , 0  , 1  ];
 julia> pred  = [0.7, 0.8, 0.9, 0.1, 0.3, 0.5, 0.6, 0.2, 0.8, 0.7];
 julia> auc(pred, label)
 0.75
@@ -22,17 +31,17 @@ julia> auc(pred, label)
 0.5
 ```
 """
-function auc(pred::AbstractArray, label::AbstractArray)
-    m = 0  # numerator
-    M = 0  # denominator
+function auc(pred::Vector{<:AbstractFloat}, label::Vector{Int})
+    m = 0.0  # numerator
+    M = 0.0  # denominator
     N = length(pred)
     for i in 1:(N-1)
         for j in (i+1):N
             if label[i] ≠ label[j]
-                M += 1
+                M += 1.0
                 if (label[i] > label[j] && pred[i] > pred[j]) ||
                    (label[i] < label[j] && pred[i] < pred[j])
-                    m += 1
+                    m += 1.0
                 elseif pred[i] == pred[j]
                     m += 0.5
                 end
@@ -43,7 +52,7 @@ function auc(pred::AbstractArray, label::AbstractArray)
 end
 
 
-function aucs2int(pred::AbstractArray, label::AbstractArray)
+function aucpair(pred::Vector{<:AbstractFloat}, label::Vector{Int})
     m = 0  # numerator
     M = 0  # denominator
     N = length(pred)
@@ -53,7 +62,7 @@ function aucs2int(pred::AbstractArray, label::AbstractArray)
                 M += 1
                 if (label[i] > label[j] && pred[i] > pred[j]) ||
                    (label[i] < label[j] && pred[i] < pred[j])
-                    m += 1
+                    m += 1.0
                 elseif pred[i] == pred[j]
                     m += 0.5
                 end
@@ -61,4 +70,65 @@ function aucs2int(pred::AbstractArray, label::AbstractArray)
         end
     end
     return m , M
+end
+
+
+
+"""
+    auc(positives::Vector{<:AbstractFloat}, negatives::Vector{<:AbstractFloat})
+
+Compute Area Under the Curve : https://arize.com/blog/what-is-auc/\n
+AUC represents the probability of positive-sample-score ≥ negative-sample-score. i.e.\n
+
+            number of (sample⁺, sample⁻) pairs that
+            satisfies sample⁺ score ≥ sample⁻ score
+    AUC = ───────────────────────────────────────────
+              count of (sample⁺, sample⁻) pairs
+
+        if sample⁺ score  > sample⁻ score, then number += 1
+        if sample⁺ score == sample⁻ score, then number += 1/2
+```
+julia> auc([0.7,0.9,0.6,0.7], [0.8,0.1,0.3,0.5,0.2,0.8])
+0.75
+
+julia> auc([0.5, 0.5, 0.5, 0.5, 0.5], [0.5, 0.5, 0.5, 0.5, 0.5])
+0.5
+
+julia> auc([0.9, 0.8, 0.7, 0.6, 0.5], [0.5, 0.6, 0.7, 0.8, 0.9])
+0.5
+```
+"""
+function auc(positives::Vector{<:AbstractFloat}, negatives::Vector{<:AbstractFloat})
+    cp = length(positives)
+    cn = length(negatives)
+    c = 0.00000 # numerator
+    C = cp * cn # denominator
+    for p in positives
+        for n in negatives
+            if p > n
+                c += 1.0
+            elseif p == n
+                c += 0.5
+            end
+        end
+    end
+    return c / C
+end
+
+
+function aucpair(positives::Vector{<:AbstractFloat}, negatives::Vector{<:AbstractFloat})
+    cp = length(positives)
+    cn = length(negatives)
+    c = 0.00000 # numerator
+    C = cp * cn # denominator
+    for p in positives
+        for n in negatives
+            if p > n
+                c += 1.0
+            elseif p == n
+                c += 0.5
+            end
+        end
+    end
+    return c , C
 end
