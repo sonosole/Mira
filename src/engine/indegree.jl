21968f23f64feaf5d7340166d521c9cbff764500
Sole <30993566+sonosole@users.noreply.github.com>
2023-03-06 00:40:07 +0800

    feat: indegree by visit fn

--- src/engine/indegree.jl
+++ src/engine/indegree.jl
@@ -1,63 +1,16 @@
 export indegree
-export indegree_by_bfs
-export indegree_by_dfs
+export resetindegree
 
 
-function indegree_by_bfs(topnode::Variable)
+function indegree(entry::Variable; by::Function=visit_by_dfs)
+    # traverse dag
+    nodes = by(entry)
+    # first zero init
     idegree = Dict{Variable,Int}()
-    queue = []
-    push!(queue, topnode)
-    setvisited(topnode)
-    while length(queue) ≠ 0
-        node = popfirst!(queue)
-        if haskid(node)
-            for kid in kidsof(node)
-                if !visited(kid)
-                    push!(queue, kid)  # once pushed,
-                    setvisited(kid)    # set visited.
-                end
-            end
-        end
-        # all zero init
+    for node in nodes
         push!(idegree, node => 0)
     end
-
-    for (node, ins) in idegree
-        unsetvisited(node)
-    end
-    return idegree
-end
-
-
-function indegree_by_dfs(topnode::Variable)
-    idegree = Dict{Variable,Int}()
-    stack = []
-    push!(stack, topnode)
-    setvisited(topnode)
-    while length(stack) ≠ 0
-        node = pop!(stack)
-        if haskid(node)
-            for kid in kidsof(node)
-                if !visited(kid)
-                    push!(stack, kid)  # once pushed,
-                    setvisited(kid)    # set visited.
-                end
-            end
-        end
-        # all zero init
-        push!(idegree, node => 0)
-    end
-
-    for (node, ins) in idegree
-        unsetvisited(node)
-    end
-    return idegree
-end
-
-
-function indegree(topnode::Variable; by::Function=indegree_by_dfs)
-    idegree = by(topnode)
-    # calculate indegree of each node
+    # then calculate indegree
     for (node, ins) in idegree
         if haskid(node)
             for kid in kidsof(node)
@@ -66,4 +19,22 @@ function indegree(topnode::Variable; by::Function=indegree_by_dfs)
         end
     end
     return idegree
+end
+
+
+function resetindegree(entry::Variable; by::Function=visit_by_dfs)
+    # traverse dag
+    nodes = by(entry)
+    # first zero init
+    for node in nodes
+        node.indegree = 0
+    end
+    # then calculate indegree
+    for node in nodes
+        if haskid(node)
+            for kid in kidsof(node)
+                kid.indegree += 1
+            end
+        end
+    end
 end
