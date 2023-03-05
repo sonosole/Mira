fa11bdf158d3c5f9187bda1145f5a86a24015d2d
Sole <30993566+sonosole@users.noreply.github.com>
2023-03-06 00:41:19 +0800

    refactor: topo sort based on indegree

--- src/engine/dagsort.jl
+++ src/engine/dagsort.jl
@@ -1,110 +1,76 @@
 export sort_by_recursive_dfs
+export sort_by_bfs
 export sort_by_dfs
-export sort_by_indegree
 
 """
-    sort_by_recursive_dfs(topnode::Variable) -> stack
+    sort_by_recursive_dfs(rootnode::Variable) -> stack
 """
 function sort_by_recursive_dfs(topnode::Variable)
     stack = Vector{Variable}()
     function visit(node::Variable)
-        setvisited(node)
+        setmarked(node)
         if haskid(node)
             for kid in kidsof(node)
-                if !visited(kid)
+                if !ismarked(kid)
                     visit(kid)
                 end
             end
         end
         push!(stack, node)
     end
-    visit(topnode)
+    visit(rootnode)
     return stack
 end
 
 
-"""
-    sort_by_dfs(topnode::Variable) -> stack
-
-nonrecursive dfs sort
-"""
-function sort_by_dfs(topnode::Variable)
-    store = Vector{Variable}()
-    stack = Vector{Variable}()
-
-    function visit(node::Variable)
-        setvisited(node)
-        for kid in kidsof(node)
-            if !visited(kid)
-                push!(stack, kid)
-            end
-        end
-    end
-
-    if !visited(topnode)
-        push!(stack, topnode)
-        lastnode = stack[end]
-    else
-        @warn "input has been visited"
-        return nothing
-    end
-
-    while length(stack)≠0
-        while haskid(lastnode)
-            len = length(stack)
-            visit(lastnode)
-            if length(stack) ≠ len
-                lastnode = stack[end]
-            else  # node has been visited
-                break
-            end
-        end
-         # the last node (leaf node) has no
-         # kids but should be marked visited
-        setvisited(lastnode)
-
-        if stack≠[]
-            push!(store, pop!(stack))
-        end
-        if length(stack)≠0
-            lastnode = stack[end]
-        end
-
-        while visited(lastnode)
-            if stack≠[]
-                push!(store, pop!(stack))
-            end
-            if length(stack)≠0
-                lastnode = stack[end]
-            else
-                break
-            end
-        end
-    end
-    return store
-end
-
 
 """
-    sort_by_indegree(topnode::Variable) -> queue
+    sort_by_bfs(rootnode::Variable) -> queue::Vector{Variable}
 """
-function sort_by_indegree(topnode::Variable; by::Function=indegree_by_bfs)
-    queue   = Vector{Variable}()
-    sorted  = Vector{Variable}()
-    idegree = indegree(topnode; by=by)
+function sort_by_bfs(entry::Variable)
+    @assert isroot(entry) "not a root node"
+    sorted = Vector{Variable}()
+    queue  = Vector{Variable}()
 
-    push!(queue, topnode)
-    while length(queue) ≠ 0
+    push!(queue, entry)
+    while notempty(queue)
         node = popfirst!(queue)
+        push!(sorted, node)
         if haskid(node)
             for kid in kidsof(node)
-                idegree[kid] -= 1
-                if idegree[kid] == 0
+                kid.indegree -= 1
+                if kid.indegree == 0
                     push!(queue, kid)
                 end
             end
         end
-        push!(sorted, node)
     end
+
+    return sorted
+end
+
+
+"""
+    sort_by_bfs(rootnode::Variable) -> stack::Vector{Variable}
+"""
+function sort_by_dfs(entry::Variable)
+    @assert isroot(entry) "not a root node"
+    sorted = Vector{Variable}()
+    stack  = Vector{Variable}()
+
+    push!(stack, entry)
+    while notempty(stack)
+        node = pop!(stack)
+        push!(sorted, node)
+        if haskid(node)
+            for kid in kidsof(node)
+                kid.indegree -= 1
+                if kid.indegree == 0
+                    push!(stack, kid)
+                end
+            end
+        end
+    end
+
     return sorted
 end
