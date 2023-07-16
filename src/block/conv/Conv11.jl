05799735d57923a56f8a074fbbc858cc4d0c033e
Sole <30993566+sonosole@users.noreply.github.com>
2023-07-17 01:25:16 +0800

    fix: Conv1x1's predict

--- src/block/conv/Conv11.jl
+++ src/block/conv/Conv11.jl
@@ -78,7 +78,7 @@ function predict(m::Conv1x1, x::AbstractArray)
     ysize = ntuple(i -> i>1 ? xsize[i] : Cₒ, D)
 
     x = reshape(x, Cᵢ, L)
-    y = f( matAddVec(w*x, b) )
+    y = f(w*x .+ b)
 
     return reshape(y, ysize)
 end
