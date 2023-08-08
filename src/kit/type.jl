c69dafe3697959ba95391a472811be030513cd4f
Sole <30993566+sonosole@users.noreply.github.com>
2023-08-09 02:19:53 +0800

    feat: export IntOrDims

--- src/kit/type.jl
+++ src/kit/type.jl
@@ -10,6 +10,9 @@ const RealOrNil = Union{Nothing, Real}
 export VecVecInt
 export VecInt
 export Dimtype
+export IntOrDims
+
 const    VecInt =        Vector{Int}
 const VecVecInt = Vector{Vector{Int}}
 const Dimtype = Union{Int,NTuple{N,Int}} where {N}
+const IntOrDims{N} = Union{Int, Dims{N}} where N
