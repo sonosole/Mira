9ba0e1ff261a4d414af91de52b76f9b21077e7b3
Sole <30993566+sonosole@users.noreply.github.com>
2023-08-26 13:48:42 +0800

    style: Ten2matFwdIter and Ten2matBwdIter
    rename some vars to disambiguate
    + Ten2matFwdIter.zsize to Ten2matFwdIter.zwidth, width refers in particular to spatial width
    + Ten2matBwdIter.zsize to Ten2matBwdIter.zrange
    + Ten2matBwdIter.sizez to Ten2matBwdIter.zwidth
    + in fwditer, (o,i) to (m,t), which refers in particular to matiters and teniters

--- src/block/conv/ten2mat.jl
+++ src/block/conv/ten2mat.jl
@@ -4,20 +4,20 @@ const IntUnitRange     = UnitRange{Int64}
 
 struct Ten2matFwdIter{D}
     stride     :: NTuple{D, Int64}  # stride of filter kernels
-    zsize      :: NTuple{D, Int64}  # shape of the output feature in n-dim conv, exclude batch and channel dims
+    zwidth     :: NTuple{D, Int64}  # spatial widths of Conv's output
     istart     :: IntStepRanges{D}  # first patch's range at each dims
     ibatchsize :: UnitRange{Int64}  # all samples indices in a batch, i.e. 1:batchsize
     ichannel   :: UnitRange{Int64}  # input channel indices, i.e. 1:channel
     irows      :: UnitRange{Int64}  # rows indices in the so-called im2col algorithm
     cols       :: Int64             # number of columns of the output in im2col algorithm
-    npatches   :: Int64             # npatches of one sample (batchsize==1)
+    npatches   :: Int64             # total patches of Conv's input
     function Ten2matFwdIter(ekernel   :: Dims{D},   # equivalent kernel sizes when dilation>1
                             dilation  :: Dims{D},   # dilation of filter kernels
                             stride    :: Dims{D},   # stride of filter kernels
-                            zsize     :: Dims{D},   # shape of the output feature in n-dim conv
+                            zwidth    :: Dims{D},   # spatial widths of Conv's output
                             rows      :: Int,       # rows of the output in im2col algorithm
                             cols      :: Int,       # cols of the output in im2col algorithm
-                            npatches  :: Int,       # npatches of one sample (batchsize==1)
+                            npatches  :: Int,       # total patches of Conv's input
                             nchannels :: Int,       # number of input channels
                             batchsize :: Int) where D
 
@@ -25,7 +25,7 @@ struct Ten2matFwdIter{D}
         SamplesRange   = 1 : batchsize
         ChannelRange   = 1 : nchannels
         RowsRange      = 1 : rows
-        new{D}(stride, zsize, InitPatchRange, SamplesRange, ChannelRange, RowsRange, cols, npatches)
+        new{D}(stride, zwidth, InitPatchRange, SamplesRange, ChannelRange, RowsRange, cols, npatches)
     end
 end
 
@@ -58,9 +58,9 @@ end
 end
 
 function Base.getindex(T::Ten2matFwdIter{D}, n::Int) where D
-    coords = batched_patch_coords(n, T.zsize)
-    # xiters is like (1:nchannels, range1, range2, ..., rangeD, 1:batchsize)
-    xiters = ntuple(D+2) do i
+    coords = batched_patch_coords(n, T.zwidth)
+    # teniters is like (1:nchannels, range1, range2, ..., rangeD, 1:batchsize)
+    teniters = ntuple(D+2) do i
         if isequal(i, 1)
             return T.ichannel
         end
@@ -72,8 +72,8 @@ function Base.getindex(T::Ten2matFwdIter{D}, n::Int) where D
         return T.ibatchsize
     end
     # length(n:steps:cols)==batchsize
-    yiters = (T.irows, n : T.npatches : T.cols)
-    return CartesianIndices(yiters), CartesianIndices(xiters)
+    matiters = (T.irows, n : T.npatches : T.cols)
+    return CartesianIndices(matiters), CartesianIndices(teniters)
 end
 
 
@@ -90,7 +90,18 @@ function Base.iterate(T::Ten2matFwdIter, i::Int=1)
 end
 
 
+"""
+    ten2matFwdInfo(x        :: AbstractArray,
+                   padding  :: Pads{D},
+                   kernel   :: Dims{D},
+                   dilation :: Dims{D},
+                   stride   :: Dims{D}) -> ROWS, COLS, batchsize, FwdIter
 
+As the name suggestes, it returns the forward informations needed when convert tensor `x` into matrix.
++ `x` is Conv's input before padding
++ `ROWS` is the number of matrix rows when `x` is converted into matrix
++ `COLS` is the number of matrix cols when `x` is converted into matrix
+"""
 function ten2matFwdInfo(x        :: AbstractArray,
                         padding  :: Pads{D},
                         kernel   :: Dims{D},
@@ -99,16 +110,16 @@ function ten2matFwdInfo(x        :: AbstractArray,
 
     assertdim(x, D+2)
     sizeofx = size(x)
-    xsize   = ntuple(i -> sizeofx[i+1] + sum(padding[i]), D)           # equivalent spatial width
+    xwidth  = ntuple(i -> sizeofx[i+1] + sum(padding[i]), D)           # equivalent input spatial width
     ekernel = ntuple(i -> dilation[i] * (kernel[i] - 1) + 1, D)        # equivalent kernel sizes
-    zsize   = ntuple(i -> (xsize[i] - ekernel[i]) ÷ stride[i] + 1, D)  # equivalent moving steps, i.e. output feature spatial width
+    zwidth  = ntuple(i -> (xwidth[i] - ekernel[i]) ÷ stride[i] + 1, D) # equivalent output spatial width
 
     xchannels = sizeofx[1]
     batchsize = sizeofx[2+D]
-    npatches  = prod(zsize)            # total moving steps along all D dims
+    npatches  = prod(zwidth)           # total moving steps along all D dims
     ROWS = xchannels * prod(kernel)    # total number of elements of a patch
-    COLS = npatches * batchsize        # total moving steps in a batch
-    Iter = Ten2matFwdIter(ekernel, dilation, stride, zsize, ROWS, COLS, npatches, xchannels, batchsize)
+    COLS = batchsize * npatches        # total moving steps in a batch
+    Iter = Ten2matFwdIter(ekernel, dilation, stride, zwidth, ROWS, COLS, npatches, xchannels, batchsize)
 
     return ROWS, COLS, batchsize, Iter
 end
@@ -132,8 +143,8 @@ function ten2mat(x        :: Array{T},
 
     xmat = similar(xten, rows, cols)
 
-    Threads.@threads for (o, i) in FwdIter
-        @inbounds xmat[o] .= reshape(xten[i], rows, batchsize)
+    Threads.@threads for (m, t) in FwdIter
+        @inbounds xmat[m] .= reshape(xten[t], rows, batchsize)
     end
 
     return xmat
@@ -143,23 +154,23 @@ end
 # (x) → ten2mat → (y);
 # (x) → ConvND  → (z);
 mutable struct Ten2matBwdIter{D}
-    zsize :: NTuple{D, UnitRange{Int64}}    # spatial range of ConvND's output
-    sizez :: NTuple{D, Int64}               # spatial width of ConvND's output
-    bools :: NTuple{D, Bool}                # parallelizable dimension is true
-    shape :: Vector{Int}                    # all unparallelizable dims forms a new idx matrix
-    total :: Int                            # total elements of aforementioned idx matrix
-    function Ten2matBwdIter(zsize::Dims{D}, bools::NTuple{D, Bool}) where D
+    zrange :: NTuple{D, UnitRange{Int64}}    # spatial range of ConvND's output
+    zwidth :: NTuple{D, Int64}               # spatial width of ConvND's output
+    bools  :: NTuple{D, Bool}                # parallelizable dimension is true
+    shape  :: Vector{Int}                    # all unparallelizable dims forms a new idx matrix
+    total  :: Int                            # total elements of aforementioned idx matrix
+    function Ten2matBwdIter(zwidth::Dims{D}, bools::NTuple{D, Bool}) where D
         count = 0
         total = 1
         shape = Vector{Int}(undef, D - sum(bools))
         for (i, parallelizable) in enumerate(bools)
             if !parallelizable
                 count += 1
-                total *= zsize[i]
-                shape[count] = zsize[i]
+                total *= zwidth[i]
+                shape[count] = zwidth[i]
             end
         end
-        new{D}(ntuple(i -> 1:zsize[i], D), zsize, bools, shape, total)
+        new{D}(ntuple(i -> 1:zwidth[i], D), zwidth, bools, shape, total)
     end
 end
 
@@ -180,7 +191,7 @@ function Base.getindex(iter::Ten2matBwdIter{D}, n::Int) where D
 
     parallelidxs = ntuple(D) do d
         if iter.bools[d]
-            return iter.zsize[d]
+            return iter.zrange[d]
         else
             counts += 1
             return coords[counts] : coords[counts]
@@ -221,8 +232,8 @@ function ten2mat(x        :: Variable{Array{T}},
 
     mat = similar(ᵛ(xten), rows, cols)
 
-    Threads.@threads for (o, i) in FwdIter
-        @inbounds mat[o] .= reshape(xten.value[i], rows, batchsize)
+    Threads.@threads for (m, t) in FwdIter
+        @inbounds mat[m] .= reshape(xten.value[t], rows, batchsize)
     end
     xmat = Variable{Array{T}}(mat, xten.backprop)
 
@@ -233,19 +244,19 @@ function ten2mat(x        :: Variable{Array{T}},
             if need2computeδ!(xten)
                 zerodelta(xten)
                 if !all(parallizable)
-                    BwdIter = Ten2matBwdIter(FwdIter.zsize, parallizable)
+                    BwdIter = Ten2matBwdIter(FwdIter.zwidth, parallizable)
                     for pindices in BwdIter
                         # locally parallel calculation
                         Threads.@threads for coords in pindices
-                            n = coords2nth(BwdIter.sizez, coords)
-                            o, i = FwdIter[n]
-                            @inbounds xten.delta[i] .+= reshape(xmat.delta[o], size(xten.delta[i]))
+                            n = coords2nth(BwdIter.zwidth, coords)
+                            m, t = FwdIter[n]
+                            @inbounds xten.delta[t] .+= reshape(xmat.delta[m], size(xten.delta[t]))
                         end
                     end
                 else
                     # globally parallel calculation
-                    Threads.@threads for (o, i) in FwdIter
-                        @inbounds xten.delta[i] .+= reshape(xmat.delta[o], size(xten.delta[i]))
+                    Threads.@threads for (m, t) in FwdIter
+                        @inbounds xten.delta[t] .+= reshape(xmat.delta[m], size(xten.delta[t]))
                     end
                 end
             end
