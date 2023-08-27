4247de2541518f736e6f67514d4e79e2a5192401
Sole <30993566+sonosole@users.noreply.github.com>
2023-08-27 18:32:04 +0800

    test: pad fns

---
+++ test/checkgrad/pad.jl
@@ -0,0 +1,31 @@
+@testset "check gradient for 1-D Paddings" begin
+    using Random
+    Random.seed!(UInt(time_ns()))
+    TYPE = Array{Float64};
+    ichannels = 2
+    timeSteps = 32
+    batchsize = 2
+    x = Variable(rand(ichannels, timeSteps, batchsize); type=TYPE)
+
+    for padfn in [padconst, padrepeat, padreflect, padsymmetric, padcircular]
+        fn(a) = padfn(a, ((0, 0), (5, 10), (0, 0)))
+        @test checkgrad(fn, x)
+    end
+end
+
+
+@testset "check gradient for 2-D Paddings" begin
+    using Random
+    Random.seed!(UInt(time_ns()))
+    TYPE = Array{Float64};
+    C = 2
+    H = 16
+    W = 16
+    B = 2
+    x = Variable(rand(C, H, W, B); type=TYPE)
+
+    for padfn in [padconst, padrepeat, padreflect, padsymmetric, padcircular]
+        fn(a) = padfn(a, ((0, 0), (5, 10),(6, 7), (0, 0)))
+        @test checkgrad(fn, x)
+    end
+end
