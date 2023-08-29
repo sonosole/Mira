ee24cff43581f82a5b72ea44cda911e51f945e81
Sole <30993566+sonosole@users.noreply.github.com>
2023-08-30 01:28:38 +0800

    feat: TransConv1d to TransConv5d

--- src/block/conv/ConvTrans.jl
+++ src/block/conv/ConvTrans.jl
@@ -4,10 +4,10 @@ export TransConv2d
 export TransConv3d
 export TransConv4d
 export TransConv5d
-
+export setoutsize
 
 """
-Applies a `D`-dim convolution over an `(D+2)`-dim input tensor of shape (ichannels, w1, w2, ..., wn, batchsize)\n
+Applies a `D`-dim transpose convolution over an `(D+2)`-dim input tensor of shape (ichannels, w1, w2, ..., wn, batchsize)\n
 # Constructor
     TransConv{D}(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
                  kernel   :: Dims{D} = ntuple(i -> 3, D),
@@ -15,14 +15,18 @@ Applies a `D`-dim convolution over an `(D+2)`-dim input tensor of shape (ichanne
                  stride   :: Dims{D} = ntuple(i -> 1, D),
                  padding  :: PadsDOrStr = "valid",
                  type     :: Type = Array{Float32}) where D
-
-+ `padmode` should be one of "zeros", "constant", "repeat", "reflect", "symmetric", "circular"
++ `ichannels` is the number of input's channels
++ `ochannels` is the number of output's channels
 + `padding` can be "valid", "same", or type `NTuple{D, Dims{2}}`
 # Detailed Processes
-+ Ordinary Conv Processes:
-    `X` → [padfn] → `Xten` → [ten2mat] → `Xmat` → [W*(∙) + B] → `Y` → [reshape] → `Z`
-+ Transpose Conv Processes:
-    `X` ← [unpad] ← `Xten` ← [mat2ten] ← `Xmat` ← [W*(∙) + B] ← `Y` ← [reshape] ← `Z`
+The transpose convolution is X = TransConv(Z), decomposed into following:
+```julia
+   ┌──────────────────────────────────────────────────────────────────┐
+   │ ┌────────────────────────┐         ┌───────────┐       ┌───────┐ │
+X ←│ │[unpad] ← Xten ← [toten]│← Xmat ← │ W*(∙) + B │ ← Y ← │reshape│ │← Z
+   │ └────────mat2ten─────────┘         └───Dense───┘       └───────┘ │
+   └──────────────────────────────────────────────────────────────────┘
+```
 """
 mutable struct TransConv{D} <: Block
     w :: VarOrNil
@@ -32,7 +36,7 @@ mutable struct TransConv{D} <: Block
     dilation :: Dims{D}
     stride   :: Dims{D}
     padding  :: Pads{D}
-    outshape :: DimsOrNil
+    outsize  :: DimsOrNil
     function TransConv{D}(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
                           kernel   :: Dims{D} = ntuple(i -> 3, D),
                           dilation :: Dims{D} = ntuple(i -> 1, D),
@@ -77,6 +81,21 @@ mutable struct TransConv{D} <: Block
 end
 
 
+function clone(this::TransConv{D}; type::Type=Array{Float32}) where D
+    cloned   = TransConv{D}()
+    cloned.w = clone(this.w, type=type)
+    cloned.b = clone(this.b, type=type)
+    cloned.f = this.f
+
+    cloned.kernel   = this.kernel
+    cloned.dilation = this.dilation
+    cloned.stride   = this.stride
+    cloned.padding  = this.padding
+    cloned.outsize  = this.outsize
+    return cloned
+end
+
+
 function Base.show(io::IO, c::TransConv{N}) where N
     P = ifelse(paddings(c.padding)==0, "", " padding=$(c.padding),")
     D = ifelse(prod(c.dilation)==1,   "", " dilation=$(c.dilation),")
@@ -88,6 +107,7 @@ function Base.show(io::IO, c::TransConv{N}) where N
     print(io, "TransConv$(N)d($ich => $och, $(c.f), kernel=$(c.kernel),$D$S$P type=$TYPE)")
 end
 
+
 function Base.show(io::IO, c::TransConv{1})
     P = ifelse(paddings(c.padding)==0, "", " padding=$(first(c.padding)),")
     D = ifelse(first(c.dilation)==1,   "", " dilation=$(first(c.dilation)),")
@@ -96,18 +116,18 @@ function Base.show(io::IO, c::TransConv{1})
     TYPE = typeof(c.w.value)
     och  = SIZE[1] ÷ prod(c.kernel)
     ich  = SIZE[2]
-    print(io, "TransConv($ich => $och, $(c.f), kernel=$(first(c.kernel)),$D$S$P type=$TYPE)")
+    print(io, "TransConv1d($ich => $och, $(c.f), kernel=$(first(c.kernel)),$D$S$P type=$TYPE)")
 end
 
 
-"""
-+ Ordinary Conv Processes:
-X → ([padfn] → Xten → [ten2mat] → Xmat → [W*(∙) + B] → Y → [reshape]) → Z
+function setoutsize(C::TransConv{D}, sz::Dims{M}) where {D,M}
+    @assert M==D+2 "TransConv$(D)d's dims of output shall be $(D+2) but got $M"
+    C.outsize = sz
+    return nothing
+end
 
-+ Transpose Conv Processes:
-X ← ([unpad] ← Xten ← [mat2ten] ← Xmat ← [W*(∙) + B] ← Y ← [reshape]) ← Z
-"""
-function forward(C::TransConv{D}, Z::Variable{T}) where {T,D}
+
+function forward(C::TransConv{D}, Z::Variable) where D
     W = C.w
     B = C.b
     N = D + 2
@@ -116,7 +136,7 @@ function forward(C::TransConv{D}, Z::Variable{T}) where {T,D}
     YSIZE = (ZSIZE[1], prod(ZSIZE[2:N]))
 
     Y = reshape(Z, YSIZE)
-    X = mat2ten(W * Y .+ B, ZSIZE, C.outshape, C.padding, C.kernel, C.dilation, C.stride)
+    X = mat2ten(W * Y .+ B, ZSIZE, C.outsize, C.padding, C.kernel, C.dilation, C.stride)
     return C.f(X)
 end
 
@@ -130,6 +150,170 @@ function predict(C::TransConv{D}, Z::AbstractArray) where D
     YSIZE = (ZSIZE[1], prod(ZSIZE[2:N]))
 
     Y = reshape(Z, YSIZE)
-    X = mat2ten(W * Y .+ B, ZSIZE, C.outshape, C.padding, C.kernel, C.dilation, C.stride)
+    X = mat2ten(W * Y .+ B, ZSIZE, C.outsize, C.padding, C.kernel, C.dilation, C.stride)
     return C.f(X)
+end
+
+
+function paramsof(c::TransConv)
+    params = Vector{Variable}(undef,2)
+    params[1] = c.w
+    params[2] = c.b
+    return params
+end
+
+
+function xparamsof(c::TransConv)
+    xparams = Vector{XVariable}(undef,2)
+    xparams[1] = ('w', c.w)
+    xparams[2] = ('b', c.b)
+    return xparams
+end
+
+function nparamsof(c::TransConv)
+    lw = length(c.w)
+    lb = length(c.b)
+    return (lw + lb)
+end
+
+elsizeof(c::TransConv) = elsizeof(c.w)
+
+function bytesof(c::TransConv, unit::String="MB")
+    n = nparamsof(c) * elsizeof(c)
+    return blocksize(n, uppercase(unit))
+end
+
+
+"""
+Applies a `1`-D transpose convolution over an `3`-D input tensor of shape (ichannels, `steps`, batchsize)\n
+
+# Constructor
+    TransConv1d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                kernel   :: Int = 3,
+                dilation :: Int = 1,
+                stride   :: Int = 1,
+                padding  :: Dims2OrStr = "valid",
+                type     :: Type = Array{Float32})
++ `ichannels` is the number of input's channels
++ `ochannels` is the number of output's channels
++ `padding` can be "valid", "same", or type `Dims{2}`
+"""
+function TransConv1d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                     kernel   :: Int = 3,
+                     dilation :: Int = 1,
+                     stride   :: Int = 1,
+                     padding  :: Pads1OrStr = "valid",
+                     type     :: Type = Array{Float32})
+
+    if !isa(padding, String)
+        padding = singletuple(padding)
+    end
+
+    kernel   = singletuple(kernel)
+    dilation = singletuple(dilation)
+    stride   = singletuple(stride)
+
+    return TransConv{1}(ichannels, ochannels, fn; kernel, dilation, stride, padding, type)
+end
+
+
+"""
+Applies a `2`-D transpose convolution over an `4`-D input tensor of shape (ichannels, `hight`, `width`, batchsize)\n
+
+# Constructor
+    TransConv2d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                kernel   :: Dims{2} = (3,3),
+                dilation :: Dims{2} = (1,1),
+                stride   :: Dims{2} = (1,1),
+                padding  :: Pads2OrStr = "valid",
+                type     :: Type = Array{Float32})
++ `ichannels` is the number of input's channels
++ `ochannels` is the number of output's channels
++ `padding` can be "valid", "same", or type `NTuple{2, Dims{2}}`
+"""
+function TransConv2d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                     kernel   :: Dims{2} = (3,3),
+                     dilation :: Dims{2} = (1,1),
+                     stride   :: Dims{2} = (1,1),
+                     padding  :: Pads2OrStr = "valid",
+                     type     :: Type = Array{Float32})
+
+    return TransConv{2}(ichannels, ochannels, fn; kernel, dilation, stride, padding, type)
+end
+
+
+
+"""
+Applies a `3`-D transpose convolution over an `5`-D input tensor of shape (ichannels, `hight`, `width`, `steps`, batchsize)\n
+
+# Constructor
+    TransConv3d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                kernel   :: Dims{3} = (3,3,3),
+                dilation :: Dims{3} = (1,1,1),
+                stride   :: Dims{3} = (1,1,1),
+                padding  :: Pads3OrStr = "valid",
+                type     :: Type = Array{Float32})
++ `ichannels` is the number of input's channels
++ `ochannels` is the number of output's channels
++ `padding` can be "valid", "same", or type `NTuple{3, Dims{2}}`
+"""
+function TransConv3d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                kernel   :: Dims{3} = (3,3,3),
+                dilation :: Dims{3} = (1,1,1),
+                stride   :: Dims{3} = (1,1,1),
+                padding  :: Pads3OrStr = "valid",
+                type     :: Type = Array{Float32})
+
+    return TransConv{3}(ichannels, ochannels, fn; kernel, dilation, stride, padding, type)
+end
+
+
+
+"""
+Applies a `4`-D transpose convolution over an `6`-D input tensor of shape (ichannels, `w1`,`w2`,`w3`,`w4`, batchsize)\n
+
+# Constructor
+    TransConv4d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                kernel   :: Dims{4} = (3,3,3,3),
+                dilation :: Dims{4} = (1,1,1,1),
+                stride   :: Dims{4} = (1,1,1,1),
+                padding  :: Pads4OrStr = "valid",
+                type     :: Type = Array{Float32})
++ `ichannels` is the number of input's channels
++ `ochannels` is the number of output's channels
++ `padding` can be "valid", "same", or type `NTuple{4, Dims{2}}`
+"""
+function TransConv4d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                     kernel   :: Dims{4} = (3,3,3,3),
+                     dilation :: Dims{4} = (1,1,1,1),
+                     stride   :: Dims{4} = (1,1,1,1),
+                     padding  :: Pads4OrStr = "valid",
+                     type     :: Type = Array{Float32})
+
+    return TransConv{4}(ichannels, ochannels, fn; kernel, dilation, stride, padding, type)
+end
+
+
+"""
+Applies a `5`-D transpose convolution over an `7`-D input tensor of shape (ichannels, `w1`,`w2`,`w3`,`w4`,`w5`, batchsize)\n
+
+# Constructor
+    TransConv5d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                kernel   :: Dims{5} = (3,3,3,3,3),
+                dilation :: Dims{5} = (1,1,1,1,1),
+                stride   :: Dims{5} = (1,1,1,1,1),
+                padding  :: Pads5OrStr = "valid",
+                type     :: Type = Array{Float32})
++ `ichannels` is the number of input's channels
++ `ochannels` is the number of output's channels
++ `padding` can be "valid", "same", or type `NTuple{5, Dims{2}}`
+"""
+function TransConv5d(ichannels::Int, ochannels::Int, fn::FunOrNil=relu;
+                     kernel   :: Dims{5} = (3,3,3,3,3),
+                     dilation :: Dims{5} = (1,1,1,1,1),
+                     stride   :: Dims{5} = (1,1,1,1,1),
+                     padding  :: Pads5OrStr = "valid",
+                     type     :: Type = Array{Float32})
+
+    return TransConv{5}(ichannels, ochannels, fn; kernel, dilation, stride, padding, type)
 end
