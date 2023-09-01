affe70148fa52acee248caf4accf4033f5eb5a60
Sole <30993566+sonosole@users.noreply.github.com>
2023-09-01 14:51:14 +0800

    feat: struct as function for all Block subtype

---
+++ src/block/0-abstract.jl
@@ -0,0 +1,27 @@
+"""
+    abstract type Block includes basic network struct like:
+    1. Dense, MLP, Linear, Affine
+    2. RNN IndRNN LSTM IndLSTM GRU IndGRU
+    3. RNNs IRNN IndRNNs RIN LSTM IndLSTMs
+    4. Conv/Conv1d/Conv2d/Conv3d/Conv4d/Conv5d/Conv1x1
+    5. TransConv/TransConv1d/TransConv2d/TransConv3d/TransConv4d/TransConv5d
+    6. Pool
+"""
+abstract type Block end
+
+function (block::Block)(x::Variable, args...; kwargs...)
+    return forward(block, x, args...; kwargs...)
+end
+
+function (block::Block)(x::AbstractArray, args...; kwargs...)
+    return predict(block, x, args...; kwargs...)
+end
+
+
+function forward(f::Function, x::Variable, args...; kwargs...)
+    return f(x, args...; kwargs...)
+end
+
+function predict(f::Function, x::AbstractArray, args...; kwargs...)
+    return f(x, args...; kwargs...)
+end
--- src/block/include.jl
+++ src/block/include.jl
@@ -1,12 +1,3 @@
-"""
-    abstract type Block includes basic network struct like:
-    1. Dense, MLP
-    2. RNN irnn IndRNN rin lstm IndLSTM
-    3. RNNs IRNN IndRNNs RIN LSTM IndLSTMs
-    4. Conv1d/Conv2d/Conv3d/Conv4d/Conv5d
-
-"""
-abstract type Block end
 export Block
 export bytesof, kbytesof, mbytesof, gbytesof, tbytesof
 export gradsof
@@ -19,6 +10,7 @@ export nops
 export checkvalues
 export staticsof
 
+include("./0-abstract.jl")
 include("./1-chain.jl")
 include("./2-residual.jl")
 include("./3-dropout.jl")
@@ -29,17 +21,3 @@ include("./conv/include.jl")
 include("./fc/include.jl")
 include("./rnn/include.jl")
 include("./pfun/include.jl")
-
-
-
-function forward(f::Function, x::Variable, args...; kwargs...)
-    return f(x, args...; kwargs...)
-end
-
-
-function predict(f::Function, x::AbstractArray, args...; kwargs...)
-    return f(x, args...; kwargs...)
-end
-
-# forward(f::Function, x::Variable)      = f(x)
-# predict(f::Function, x::AbstractArray) = f(x)
