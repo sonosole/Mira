d2891f9ade86e8e026c346924f7d70dc8320e093
Sole <30993566+sonosole@users.noreply.github.com>
2023-09-09 22:04:14 +0800

    feat: add FusedAttention block
    for features fusing or selecting, or even incremental/decrement learning

--- src/block/att/fused.jl
+++ src/block/att/fused.jl
@@ -1,31 +1,177 @@
 export FusedAttention
-
 const QKVChanger = Union{Function, Nothing, Block}
 
 mutable struct FusedAttention <: Block
-    Qf   :: ComposedFunction
-    Kf   :: ComposedFunction
-    Vf   :: ComposedFunction
+    Qf :: ComposedFunction
+    Kf :: ComposedFunction
+    Vf :: ComposedFunction
     function FusedAttention(kinner::QKVChanger,
                             kouter::QKVChanger,
                             vinner::QKVChanger,
-                            kouter::QKVChanger)
-        Fq = kinner ∘ kouter
-        Fk = kinner ∘ kouter
-        Fv = vinner ∘ vouter
+                            vouter::QKVChanger)
+        Fq = kouter ∘ kinner
+        Fk = kouter ∘ kinner
+        Fv = vouter ∘ vinner
         new(Fq, Fk, Fv)
     end
 end
 
 function Base.show(io::IO, ::MIME"text/plain", f::FusedAttention)
     println("FusedAttention: ")
-    println("   WQ is ", f.Wq)
-    println("   WK is ", f.Wk)
-    println("   WV is ", f.Wv)
+    println("   Query Operator: ", f.Qf.outer, " ∘ ",f.Qf.inner)
+    println("   Key   Operator: ", f.Kf.outer, " ∘ ",f.Kf.inner)
+    println("   Value Operator: ", f.Vf.outer, " ∘ ",f.Vf.inner)
 end
 
 
 function forward(att::FusedAttention, xs::Vector{Variable{T}}) where T
+    # first we produce xs using any ComposedFunction e.g.
+    # 1. Conv1x1 ∘ Pool3d ∘ softmax, for 3-Dim tensor
+    # 2. Conv2d ∘ nothing, with strides > 1 for dowsampling, and smaller output channel dimensions
+    # 3. nothing ∘ nothing, just keeping the original data
+    L  = length(xs)
+    Qs = Vector{Variable{T}}(undef, L)
+    Ks = Vector{Variable{T}}(undef, L)
+    Vs = Vector{Variable{T}}(undef, L)
+    Threads.@threads for i in 1:L
+        Qs[i] = att.Qf(xs[i])
+        Ks[i] = att.Kf(xs[i])
+        Vs[i] = att.Vf(xs[i])
+    end
+
+    S = size(first(Vs))        # backup for recovering shape
+    N = ndims(first(Vs))       # ─┬─►  N may equal to D, if elements in Qs Ks
+    may_reshape_qkv_to_3d(Qs)  #  │    and Vs have more then two dimensions.
+    may_reshape_qkv_to_3d(Ks)  #  │    in this case, may_reshape_qkv_to_3d fn
+    may_reshape_qkv_to_3d(Vs)  #  │    would do nothing to elements in Qs, Ks
+    D = ndims(first(Vs))       # ─┘    and Vs.
+
+    sizek = size(first(Ks))
+    sizev = size(first(Vs))
+    widthk = prod(sizek[2:D-1])  # ∏ⱼWᴷⱼ, j in 2:D-1
+    widthv = prod(sizev[2:D-1])  # ∏ⱼWⱽⱼ, j in 2:D-1
+    batch  = sizev[D]     # batchsize
+    dᴷ = first(sizek)     # query dims
+    dⱽ = first(sizev)     # value dims
+
+    # Suppose each input has size like (C, W₁,W₂,...,Wₘ, B), we will later
+    # reshape each of them into (C*∏ⱼWⱼ,1,B)，just like embedding slice in NLP
+    kshape = ntuple(3) do i
+        isequal(i, 1) && return widthk * dᴷ
+        isequal(i, 2) && return 1
+        return batch
+    end
+    vshape = ntuple(3) do i
+        isequal(i, 1) && return widthv * dⱽ
+        isequal(i, 2) && return 1
+        return batch
+    end
+
+    Threads.@threads for i in 1:L
+        Qs[i] = reshape(Qs[i], kshape)
+        Ks[i] = reshape(Ks[i], kshape)
+        Vs[i] = reshape(Vs[i], vshape)
+    end
+
+    # concate L inputs of size (C*∏ⱼWⱼ, 1, B) into a
+    # whole tensor with size   (C*∏ⱼWⱼ, L, B), then split it
+    # into B tensors of size   (C*∏ⱼWⱼ, L)
+    Q = chunk(cat(Qs, dims=2), dim=3)
+    K = chunk(cat(Ks, dims=2), dim=3)
+    V = chunk(cat(Vs, dims=2), dim=3)
+
+    αs = Vector{Variable{T}}(undef, batch)  # attention matrices
+    ys = Vector{Variable{T}}(undef, batch)  # fused samples
+    Threads.@threads for b in 1:batch
+        score = softmax(K[b]' * Q[b] / sqrt(dᴷ), dims=1)
+        αs[b] = mean(score, dims=2)
+        ys[b] = reshape(V[b] * αs[b], widthv * dⱽ, 1, 1)
+    end
+
+    shape = ntuple(N) do i
+        isequal(i, 1) && return dⱽ
+        isequal(i, D) && return batch
+        return S[i]
+    end
+    return reshape(cat(ys, dims=3), shape)
+end
+
+
+function predict(att::FusedAttention, xs::Vector{T}) where T <: AbstractArray
+    # first we produce xs using any ComposedFunction e.g.
+    # 1. Conv1x1 ∘ Pool3d ∘ softmax, for 3-Dim tensor
+    # 2. Conv2d ∘ nothing, with strides > 1 for dowsampling, and smaller output channel dimensions
+    # 3. nothing ∘ nothing, just keeping the original data
+    L  = length(xs)
+    Qs = Vector{AbstractArray}(undef, L)
+    Ks = Vector{AbstractArray}(undef, L)
+    Vs = Vector{AbstractArray}(undef, L)
+    Threads.@threads for i in 1:L
+        Qs[i] = att.Qf(xs[i])
+        Ks[i] = att.Kf(xs[i])
+        Vs[i] = att.Vf(xs[i])
+    end
+
+    S = size(first(Vs))        # backup for recovering shape
+    N = ndims(first(Vs))       # ─┬─►  N may equal to D, if elements in Qs Ks
+    may_reshape_qkv_to_3d(Qs)  #  │    and Vs have more then two dimensions.
+    may_reshape_qkv_to_3d(Ks)  #  │    in this case, may_reshape_qkv_to_3d fn
+    may_reshape_qkv_to_3d(Vs)  #  │    would do nothing to elements in Qs, Ks
+    D = ndims(first(Vs))       # ─┘    and Vs.
+
+    sizek = size(first(Ks))
+    sizev = size(first(Vs))
+    widthk = prod(sizek[2:D-1])  # ∏ⱼWᴷⱼ, j in 2:D-1
+    widthv = prod(sizev[2:D-1])  # ∏ⱼWⱽⱼ, j in 2:D-1
+    batch  = sizev[D]     # batchsize
+    dᴷ = first(sizek)     # query dims
+    dⱽ = first(sizev)     # value dims
+
+    # Suppose each input has size like (C, W₁,W₂,...,Wₘ, B), we will later
+    # reshape each of them into (C*∏ⱼWⱼ,1,B)，just like embedding slice in NLP
+    kshape = ntuple(3) do i
+        isequal(i, 1) && return widthk * dᴷ
+        isequal(i, 2) && return 1
+        return batch
+    end
+    vshape = ntuple(3) do i
+        isequal(i, 1) && return widthv * dⱽ
+        isequal(i, 2) && return 1
+        return batch
+    end
+
+    Threads.@threads for i in 1:L
+        Qs[i] = reshape(Qs[i], kshape)
+        Ks[i] = reshape(Ks[i], kshape)
+        Vs[i] = reshape(Vs[i], vshape)
+    end
+
+    # concate L inputs of size (C*∏ⱼWⱼ, 1, B) into a
+    # whole tensor with size   (C*∏ⱼWⱼ, L, B), then split it
+    # into B tensors of size   (C*∏ⱼWⱼ, L)
+    Q = chunk(cat(Qs, dims=2), dim=3)
+    K = chunk(cat(Ks, dims=2), dim=3)
+    V = chunk(cat(Vs, dims=2), dim=3)
+
+    αs = Vector{AbstractArray}(undef, batch)  # attention matrices
+    ys = Vector{AbstractArray}(undef, batch)  # fused samples
+    d⁻¹= eltype(T)(1 / sqrt(dᴷ))
+    Threads.@threads for b in 1:batch
+        score = softmax(K[b]' * Q[b] .* d⁻¹, dims=1)
+        αs[b] = mean(score, dims=2)
+        ys[b] = reshape(V[b] * αs[b], widthv * dⱽ, 1, 1)
+    end
+
+    shape = ntuple(N) do i
+        isequal(i, 1) && return dⱽ
+        isequal(i, D) && return batch
+        return S[i]
+    end
+    return reshape(cat(ys, dims=3), shape)
+end
+
+
+function may_reshape_qkv_to_3d(xs::Union{Vector{Variable{T}}, Vector{S}}) where {T,S <: AbstractArray}
     L = length(xs)
     x = first(xs)
     N = ndims(x)
@@ -43,56 +189,5 @@ function forward(att::FusedAttention, xs::Vector{Variable{T}}) where T
             xs[i] = reshape(xs[i], C, 1, B)
         end
     end
-    D = ndims(first(xs))
-
-    sizex = size(first(xs))     # sizex
-    width = prod(sizex[2:D-1])  # ∏ⱼWⱼ
-    batch = sizex[D]            # batchsize
-    dᴷ = att.kdim               # query dims
-    dⱽ = att.vdim               # value dims
-
-    # Suppose each input has size like (C, W₁,W₂,...,Wₘ, B), we will later
-    # reshape each of them into (C*∏ⱼWⱼ,1,B)，just like embedding slice in NLP
-    kshape = ntuple(3) do i
-        isequal(i, 1) && return width * dᴷ
-        isequal(i, 2) && return 1
-        return batch
-    end
-    vshape = ntuple(3) do i
-        isequal(i, 1) && return width * dⱽ
-        isequal(i, 2) && return 1
-        return batch
-    end
-
-    Qs = Vector{Variable{T}}(undef, L)
-    Ks = Vector{Variable{T}}(undef, L)
-    Vs = Vector{Variable{T}}(undef, L)
-    Threads.@threads for i in 1:L
-        Qs[i] = reshape(att.Qf(xs[i]), kshape)
-        Ks[i] = reshape(att.Kf(xs[i]), kshape)
-        Vs[i] = reshape(att.Vf(xs[i]), vshape)
-    end
-
-    # concate L inputs, each with size (C*∏ⱼWⱼ,1,B), into a
-    # whole tensor whose size is (C*∏ⱼWⱼ,L,B), then split it
-    # into #B tensors, each with size (C*∏ⱼWⱼ, L)
-    Q = chunk(cat(Qs, dims=2), dim=3)
-    K = chunk(cat(Ks, dims=2), dim=3)
-    V = chunk(cat(Vs, dims=2), dim=3)
-
-    αs = Vector{Variable{T}}(undef, batch)  # attention matrices
-    ys = Vector{Variable{T}}(undef, batch)  # fused samples
-    Threads.@threads for b in 1:batch
-        score = softmax(K[b]' * Q[b] / sqrt(dᴷ), dims=1)
-        αs[b] = mean(score, dims=2)
-        ys[b] = reshape(V[b] * αs[b], width*dⱽ, 1, 1)
-    end
-
-    shape = ntuple(N) do i
-        isequal(i, 1) && return dⱽ
-        isequal(i, D) && return batch
-        return sizex[i]
-    end
-    println(shape)
-    return reshape(cat(ys, dims=3), shape), αs
+    return nothing
 end
---
+++ src/block/att/include.jl
@@ -0,0 +1,1 @@
+include("./fused.jl")
