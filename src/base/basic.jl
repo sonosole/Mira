64589726ee28466cbc329f8c77d1eb1e5ae5d78c
Sole <30993566+sonosole@users.noreply.github.com>
2023-09-16 16:32:21 +0800

    feat: onehot fn

--- src/base/basic.jl
+++ src/base/basic.jl
@@ -1,6 +1,8 @@
 export Zeros
 export Ones
 export vartype
+export onehot
+
 
 """
     Zeros(::Type{T}, shape...) where T
@@ -36,4 +38,36 @@ end
     @assert T1 <: T2 || T1 >: T2
     T = T1 <: T2 ? T2 : T1
     return T
+end
+
+
+function onehot(i::Int, C::Int; type::Type=Array{Float32})
+    x = Zeros(type, C, 1)
+    x[i] = 1
+    return x
+end
+
+"""
+    onehot(labels::Vector{Int}, C::Int; type::Type=Array{Float32}) -> x::Type
++ `labels` is a batched labels, contains categories classification label
++ `C` is the nameber of categories, aka #dimentions of label
++ `type` is the returned var's type
+
+# example
+```
+julia> onehot([1,2,3,4,3,2,1], 4, type=Array{Int})
+4×7 Matrix{Int64}:
+ 1  0  0  0  0  0  1
+ 0  1  0  0  0  1  0
+ 0  0  1  0  1  0  0
+ 0  0  0  1  0  0  0
+```
+"""
+function onehot(labels::Vector{Int}, C::Int; type::Type=Array{Float32})
+    B = length(labels)
+    x = Zeros(type, C, B)
+    for (j, i) in enumerate(labels)
+        x[i,j] = 1
+    end
+    return x
 end
